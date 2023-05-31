6af83de287d0b3735761d889773254e7e4681628
DucGiangBui <2051060481@e.tlu.edu.vn>
2023-05-31 12:10:23 +0700

    Update

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,3 +16,16 @@ function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+
+window.addEventListener('load', function() {
+  // Tạo một mảng chứa các class cho các skill-item
+  var skillClasses = ['icon-primary', 'icon-secondary', 'icon-tertiary'];
+
+  // Lặp qua tất cả các skill-item
+  var skillItems = document.querySelectorAll('.skill-item');
+  for (var i = 0; i < skillItems.length; i++) {
+    // Thêm một class ngẫu nhiên từ mảng skillClasses
+    var randomClass = skillClasses[Math.floor(Math.random() * skillClasses.length)];
+    skillItems[i].classList.add(randomClass);
+  }
+});
